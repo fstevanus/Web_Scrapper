5f6ec9d122468d60f94ebe946ca9662b80712589
Fajar Stevanus <dracbros@gmail.com>
2018-09-18 13:19:49 +0700

    sorry tadi salah wkwkwk

--- scrap.py
+++ scrap.py
@@ -3,14 +3,9 @@ from sqlite3 import Error
 import urllib2
 from bs4 import BeautifulSoup
 
-def create_connection(dbFile):
-    try:
-        conn = sqlite3.connect(dbFile)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
+
 
 if __name__ == '__main__':
-    create_connection("/home/fs/python_project/scrap")
+    conn = sqlite3.connect("/home/fs/python_project/scrap") #ganti dengan lokasi connect bro
+
+    conn.close()
